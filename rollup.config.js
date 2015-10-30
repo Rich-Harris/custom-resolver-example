aed6973b5e488051e29906c02a69d9d564de8ba2
Rich-Harris <richard.a.harris@gmail.com>
2015-10-29 23:30:33 -0400

    add explanation

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ export default {
 	plugins: [{
 		resolveId: function ( importee, importer ) {
 			if ( importee[0] === '@' ) return resolve( 'src', importee.slice( 2 ) );
+			// if nothing is returned, we fall back to default resolution
 		}
 	}]
 };
